3d64a50b324413c85d7c100f70730e3812e7a98d
yassen717 <EngYassenIbrahim@gmail.com>
2026-01-07 18:59:42 +0200

    feat(mbti): add server-side search on Home page and UI polish for charts

--- mbti/views.py
+++ mbti/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 from .models import MBTIType
 
+
 def home(request):
-    mbti_types = MBTIType.objects.all()
-    return render(request, 'pages/home.html', {'mbti_types': mbti_types})
+    q = request.GET.get('q', '').strip()
+    mbti_types = MBTIType.objects.filter(Q(name__icontains=q) | Q(description__icontains=q)) if q else MBTIType.objects.all()
+    return render(request, 'pages/home.html', {'mbti_types': mbti_types, 'q': q})
+
 
 def mbti_detail(request, pk):
     mbti = get_object_or_404(MBTIType, pk=pk)
     return render(request, 'pages/mbti_detail.html', {'mbti': mbti})
+
 
 def about(request):
     return render(request, 'pages/about.html')
